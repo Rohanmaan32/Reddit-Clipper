82f2862ac162b754c34fe0aa85b157d29073ef10
Rohan Maan <rohanmaan1212@gmail.com>
2022-06-10 16:49:40 +0530

    fixes and re organisation, backing to a notebook

--- Downloader.py
+++ Downloader.py
@@ -3,7 +3,7 @@ import urllib.request
 import progressbar
 
 pbar=None
-def download(url,folder):
+def download(url,folder,filename):
     
     if url[-1]=='/':
         url=url.rstrip('/')
@@ -14,7 +14,7 @@ def download(url,folder):
     Nsfw_filter=response.json()[0]['data']['children'][0]['data']['over_18']
     if Nsfw_filter is False:
         print(Video_Url)
-        urllib.request.urlretrieve(Video_Url, f'Downloads/{folder}/File.mp4',show_progress) 
+        urllib.request.urlretrieve(Video_Url, f'{folder}/{filename}.mp4',show_progress) 
         
 #NOT MY CODE got from stackoverflow
 def show_progress(block_num, block_size, total_size):
@@ -29,4 +29,17 @@ def show_progress(block_num, block_size, total_size):
     else:
         pbar.finish()
         pbar = None
-#download('https://www.reddit.com/r/Funnymemes/comments/usuw7b/exactly/')
+"""
+So it looks like reddit wont give you the video and audio together, so thats gonna need combining
+video is  (v.reddit/jsdnfkj) URL + DASH_480.mp4?source=fallback
+audio is  TRICKIER IT SEEMS 
+sometimes (v.reddit/sdujfhd) URl + DASH_audio.mp4    https://v.redd.it/x228x4b25h491/DASH_audio.mp4?source=fallback
+othertimes https://v.redd.it/z08avb339n801/audio?source=fallback
+maybe other ways too
+audio is handled weirdly, but most annoyingly reddit straight up refuses v.redd.it/,,/.json
+that just fails
+but this method seems to sorta work but resolution is the problem
+the url plus the DASH_res i need to guess the resolution ig. not too challenging. start at 1080, try and except to 720,480,\
+otherwise continue but there must be a better way than this"""
+
+download('https://www.reddit.com/r/aww/comments/v7x39u/man_stops_to_rescue_kitten_gets_ambushed_by/','Downloads/test','test1')
--- Reddit_Sql.py
+++ Reddit_Sql.py
@@ -1,35 +1,11 @@
-import mysql.connector as ms
-
-
 #params={"subreddit":"aww"}
 #req=requests.get("https://api.pushshift.io/reddit/subreddit/search",headers=params)
 #print(req.content)
 
-
-
-mydb=ms.connect(host="localhost",user="root",passwd="1111",database="reddit")
-if mydb.is_connected():
-    print("Successfully connected")
-else:
-    print("Not connected")
-cr=mydb.cursor()
-#give a list of subreddits to take all videos and push to a
-def RetrieveAndSqlInsert(SubredditList):
+def RetrieveAndDownload(SubredditList):
     fails=0
     for i in SubredditList:
         for j in reddit.subreddit(i).hot(limit=10000):
             if j.url[8].lower()=='v':
                 #print(j.url[18:31])
-                try:
-                    cr.execute(f'insert into list1 values("{j.url[18:31]}","{SubredditList}");')
-                    mydb.commit()
-                except Exception as e:
-                    print(e)
-                    fails+=1
-                    if fails>50:
-                       break 
-                    else:
-                        continue
-        if fails>50:
-            break
-    print(f"SQL ENTRY DONE with {fails} fails")
+            
